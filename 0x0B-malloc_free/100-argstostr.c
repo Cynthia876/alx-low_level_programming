854d87963f82bf6bd5a077c5e709dbe0065c35c9
Cynthia876 <khaycy876@gmail.com>
2023-11-03 07:42:30 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-* argstostr - concatenates all the arguments of your program.
-* @ac: arguments count
-* @av: arguments vector
-*
-* Return: a pointer to a new string, or NULL if it fails
-*/
+ * argstostr - Main Entry
+ * @ac: input
+ * @av: input
+ * Return: 0
+ */
 char *argstostr(int ac, char **av)
 {
-  char *str, *s;
-  int i, j, k, len = 0;
+	int i, n, k = 0, len = 0;
+	char *str;
 
-  if (ac == 0 || av == NULL)
-     return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-  for (i = 0; i < ac; i++)
-  {
-     s = av[i];
-     j = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
 
-     while (s[j++])
-        len++;
-     len++;
-  }
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
 
-  str = (char *)malloc(sizeof(char) * (len + 1));
-  if (str == NULL)
-     return (NULL);
-
-  for (i = 0, j = 0; i < ac && j < len; i++)
-  {
-     s = av[i];
-     k = 0;
-
-     while (s[k])
-     {
-        str[j] = s[k];
-        k++;
-        j++;
-     }
-     str[j++] = '\n';
-  }
-  str[j] = '\0';
-
-  return (str);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+	return (str);
 }
-
