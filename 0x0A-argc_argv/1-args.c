451507f0c06bbe7f5c7e9f0efb9f625a242dd9a9
Cynthia876 <khaycy876@gmail.com>
2023-11-03 02:09:10 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
-		
- * main - print the number of arguments passed to the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: Always 0 (Success)
-		
- */
-		
-
-int main(int argc, char *argv[])
-		
+* main -  prints all arguements
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
+**/
+int main(int argc, char **argv)
 {
-		
-(void) argv; /*Ignore argv*/
+int i;
 
-	printf("%d\n", argc - 1);
-	return (0);		
+
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+
+
+return (0);
 }
 
